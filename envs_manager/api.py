5eb8f614fdcb4e8041dd992ad64fb2fd369b788c
Carlos Cordoba <ccordoba12@gmail.com>
2025-05-25 12:37:11 -0500

    Re-add api module with objects that are needed in the frontend

---
+++ envs_manager/api.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2022-present Spyder Development Team and envs-manager contributors
+#
+# SPDX-License-Identifier: MIT
+
+from .manager import Manager, ManagerActionResult, ManagerActions, ManagerOptions
