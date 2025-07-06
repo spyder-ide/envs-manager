eb2b30e732ed0ea716eb1461e428849bd8cc3c18
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-07-06 18:02:16 -0300

    fix: add jupyter server missing extensions point

--- envs_manager/__init__.py
+++ envs_manager/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2022-present Spyder Development Team and envs-manager contributors
 #
 # SPDX-License-Identifier: MIT
+from envs_manager.jupyter import EnvManagerApp
+
+def _jupyter_server_extension_points():
+    """
+    Returns a list of dictionaries with metadata describing
+    where to find the `_load_jupyter_server_extension` function.
+    """
+    return [{"module": "envs_manager.jupyter", "app": EnvManagerApp}]
