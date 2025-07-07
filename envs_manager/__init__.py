0b91ea583a02969c335e8903961aa8ab1934397f
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-07-07 14:32:06 -0300

    feat: set jupyter-server as optional dependency

--- envs_manager/__init__.py
+++ envs_manager/__init__.py
@@ -1,11 +1,13 @@
 # SPDX-FileCopyrightText: 2022-present Spyder Development Team and envs-manager contributors
 #
 # SPDX-License-Identifier: MIT
-from envs_manager.jupyter import EnvManagerApp
+
 
 def _jupyter_server_extension_points():
     """
     Returns a list of dictionaries with metadata describing
     where to find the `_load_jupyter_server_extension` function.
     """
+    from envs_manager.jupyter import EnvManagerApp
+
     return [{"module": "envs_manager.jupyter", "app": EnvManagerApp}]
